9f619037c4d63ead15cf3b5716effc41a77ecfc5
Santhi A <santhi@redplanetconsulting.com>
2021-04-08 00:32:43 +0530

    Base GUI

--- Custom Tools/js/index.js
+++ Custom Tools/js/index.js
@@ -1,11 +1,39 @@
 $(document).ready(function () {
   var map;
+  var titleMapping = {
+    "tool-bm": "BookMark",
+    "tool-draw": "Draw"
+  }
   require(["esri/map"],
     function (Map) {
+
       map = new Map("map-div", {
         basemap: "topo",
         center: [-122.45, 37.75],
         zoom: 13
       });
+
+      $(".upside a").click(function() {
+        var clickedElem = $(this);
+        $(".upside a").removeClass("active-tool");
+        clickedElem.addClass("active-tool");
+        openNav();
+        $(".side-nav-header h3").text(titleMapping[clickedElem.prop("id")]);
+      });
+
+      function openNav() {
+        $("#map-container").css("width", "70%");
+        $("#tools-content-container").css("width", "29%");
+      }
+
+      $(".side-nav-header svg").click(function() {
+        $(".upside a").removeClass("active-tool");
+        closeNav();
+      })
+
+      function closeNav() {
+        $("#map-container").css("width", "100%");
+        $("#tools-content-container").css("width", "0%");
+      }
     });
 })
